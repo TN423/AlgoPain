601087e6bfc5b54b876b3f0c8a8af969b4d25da5
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-10-10 16:29:15 -0400

    Time: 190 ms (18.39%), Space: 99.1 MB (24.96%) - LeetHub

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+//write base case first - if no length then return null for the node
+
+//preorder to figure out current root and inorder to figure out the left/right
+//find pivot in inorder - the left and right will always be to left and right side of pivot 
+var buildTree = function(preorder, inorder) {
+    if (!inorder.length) return null
+    var root = preorder.shift()
+    var pivot = inorder.indexOf(root)
+    var node = new TreeNode(root)
+    node.left =  buildTree(preorder, inorder.slice(0,pivot))
+    node.right = buildTree(preorder, inorder.slice(pivot+1))
+    return node
+};
