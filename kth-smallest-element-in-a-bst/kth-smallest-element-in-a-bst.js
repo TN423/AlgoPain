c3f555db56f7078eec0e6dedfee28bd79ce3432a
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-10-11 15:23:15 -0400

    Time: 80 ms (92.87%), Space: 46 MB (11.36%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
@@ -11,21 +11,24 @@
  * @param {number} k
  * @return {number}
  */
+
+ // var sortedNodes = dfs(root).sort((a,b)=>{return a-b}) -if you used preorder (puting push before recursing .left in dfs), then you would have to sort. But because we put the push in between the .left and .right, the result array is already sorted
 var kthSmallest = function(root, k) {
-    var sortedNodes = dfs(root).sort((a,b)=>{return a-b})
-    console.log(sortedNodes)
-    var result = sortedNodes[k-1]
-    return result
+    var sortedNodes = dfs(root)
+    return sortedNodes[k-1]
 };
 
+//do inorder traversal (put the push after the call to node .left)
+//this way the result array is already sorted 
 function dfs (root) {
     var result = []
-    
-    function recurse (node) {
-        result.push(node.val)
+      function recurse (node) {
         if (node.left) recurse(node.left)
+        result.push(node.val)
         if (node.right) recurse(node.right)
-    }
+      }
     recurse(root)
     return result
 }
+
+//better
