336556b1fc3eff6623b7c8d99d29ad7c67942507
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-10-15 16:00:04 -0400

    Time: 196 ms (5.09%), Space: 44.2 MB (5.23%) - LeetHub

--- clone-graph/clone-graph.js
+++ clone-graph/clone-graph.js
@@ -15,23 +15,39 @@
     //keep track of nodes visited
     //keep track of 
 
-var cloneGraph = function(node) {
-   
-   
-   function dfs (root) {
-     if (root === null) return null
-     if (visited[root.val]) return visited[root.val]
-     var clone = new Node(root.val)
-     visited[clone.val]=clone
-     for (var edge of root.neighbors) {
-       clone.neighbors.push(dfs(edge))
-     }
-      console.log(clone)
-     return clone  
-   }
-    var visited = {} 
-    return dfs(node)
-};
+var cloneGraph = function (graph) {
+    if (!graph) return null
+    var map = new Map()
+    function dfs (node) {
+        if(map.has(node)) return map.get(node)
+        var clone = new Node(node.val)
+        map.set(node, clone)
+        //for of loop since map is an iterable colleciton
+        for (var neighbor of node.neighbors) {
+            clone.neighbors.push(dfs(neighbor))
+        }
+        console.log(clone)
+        return clone
+    }
+    return dfs(graph)
+}
+
+// var cloneGraph = function(node) {
+//     var visited = {} 
+//     function dfs (root) {
+//      if (root === null) return null
+//      if (visited[root.val]) return visited[root.val]
+//      var clone = new Node(root.val)
+//      visited[clone.val]=clone
+//      for (var edge of root.neighbors) {
+//        clone.neighbors.push(dfs(edge))
+//      }
+//       console.log(clone)
+//      return clone  
+//    }
+
+//     return dfs(node)
+// };
 
 
 
