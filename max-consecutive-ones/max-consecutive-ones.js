ce30f0d13bd871af286ef53fa827f4e6e7ae849b
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-10-20 18:22:53 -0400

    Time: 1514 ms (5.07%), Space: 41.2 MB (67.20%) - LeetHub

---
+++ max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+  if (nums.length ===1 && nums[0]===1) return 1
+  
+  var currMax = 0
+  var absMax =0
+  for (var i=0; i <nums.length;i++) {
+      var start=i
+      currMax = nums[start]
+      var end =i+1
+      while (nums[start]===nums[end]) {
+          currMax+=nums[end]
+          end++
+      }
+      absMax=Math.max(absMax,currMax)
+  }
+    return absMax
+};
