6a30263ee3059339277064fe85f7ba2ccb9350db
Justin <hurstjustina@gmail.com>
2021-10-22 03:13:18 -0400

    doubleLinkList

--- linkedList/doubeLinkedList.js
+++ linkedList/doubeLinkedList.js
@@ -25,5 +25,74 @@ class doubleList () {
       newNode.prev = this.tail
       this.tail = newNode
     }
+    this.length++
+    return this
   }
+
+  pop () {
+    var current = this.tail
+    this.tail = this.tail.prev
+    this.tail.next = null
+    current.prev = null
+    this.length --
+    return current
+  }
+
+  shift () {
+    var oldHead = this.head
+    this.head = this.head.next
+    this.head.prev = null
+    oldHead.next = null
+  }
+
+  unshift (val) {
+    var oldHead = this.head
+    this.head = new Node (val)
+    oldHead.prev = this.head
+    this.head.next = oldHead
+  }
+
+  get (position) {
+    var current;
+    var count;
+    if (position <= this.length/2) {
+      var current = this.head
+      var count =0
+      while (count !== position) {
+        current = this.head.next
+        count++
+      }
+    } else {
+      current = this.tail
+      var count = this.length
+      while (count !== position) {
+        current = this.tail.prev
+        count --
+      }
+    }
+    return current
+  }
+
+  set (val,position) {
+    var found = this.get(position)
+    if (found !== null) {
+      found.val = val
+      return true
+    }
+    return false
+  }
+
+  insert (val, position) {
+    //keep track of the before portion and the after portion for where the node will be inserted
+    var beforeNode = this.get(position -1)
+    var afterNode = beforeNode.next
+    var NewNode = new Node(val)
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    newNode.next = afterNOde
+    afterNode.pre = newNode
+  }
+
+
+
 }
