8bfb0b25fb8d9db81fdd3f17da344b1cbc339642
Justin <hurstjustina@gmail.com>
2021-10-21 20:16:25 -0400

    reverseLinkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -60,5 +60,108 @@ class LinkedList {
     this.length --
     return current
   }
+
+  unshift (val) {
+    if (!this.head) {
+      this.head = new Node(vale)
+      this.tail = this.head
+    }
+    var newHead = new Node(val)
+    var newhead.next = this.head
+    this.head = newHead
+    this.length++
+    return this
+  }
+  //takes a number and returns the item in that position
+  //traverse the list
+  //while
+  get (val) {
+    var count = 0
+    var current = this.head
+    //travers
+    while (count !== val){
+      current = current.next
+      count++
+    }
+    return current
+  }
+  set (newVal, position) {
+    var count = 0
+    var current = this.head
+    //alternatively use get method, then set the returned nodes .val property to newVal
+    while (count !== position){
+      current = this.head.next
+      count++
+    }
+    current.val = newVal
+    return current
+  }
+
+  //add a node at a specific position
+  //better strategy is to use prev = this.get(index-1), then temp = prev.next
+  insert (val, position) {
+    //create a new node
+    var newNode = new Node(val)
+    var counter1 = 0
+    var counter2 = 0
+    var currentPost = this.head
+    var currentPre = this.head
+    //if want to insert at position 4, the travers to position 5
+    while(counter1 !== (position+1)) {
+      currentPost = this.head.next
+      counter1+=1
+    }
+    newNode.next = currentPost
+    while (counter2 !== (position -1)) {
+      currentPre = this.head.next
+      counter2+=1
+    }
+    currentPre.next = newNode
+    //set position 5 node as newNodes.next
+    //traverse to position 3
+      // change current node.next to be new node
+    return this;
+  }
+
+  remove(position) {
+    var prePoint = this.get(position-1)
+    var removed = prePoint.next
+    var postPoint = this.get (position+1)
+    prePoint.next = postPoint
+    return removed
+  }
+
+  //reversed in place. don't make a copy
+  //traverse and reverse
+  reverseList () {
+    var list = this.head
+    var revList = null
+    while (list) {
+      if (revList === null){
+        revList = this.tail
+      } else {
+        revList.next = this.tail
+      }
+      list.pop()
+    }
+    return revList
+  }
+
+  reverse2 () {
+    var node = this.head
+    this.head = this.tail
+    this.tail = node
+    var next
+    var prev = null
+    for (var i = 0; i <this.length;i++) {
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this
+  }
+
+
 }
 
