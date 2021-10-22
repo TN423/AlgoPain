7e703ef9c7f4df9908bfded9ea9c167616ba9e94
Justin <hurstjustina@gmail.com>
2021-10-21 23:24:55 -0400

    doubleLinkList

---
+++ linkedList/doubeLinkedList.js
@@ -0,0 +1,29 @@
+class Node () {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+
+}
+
+class doubleList () {
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push (val) {
+    var newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      //the second link going backward
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+  }
+}
