58c76a092790690129542a160cc8efbf32644654
Justin <hurstjustina@gmail.com>
2021-10-25 18:17:47 -0400

    maxBinHeap

--- binaryHeap/maxBinHeap.js
+++ binaryHeap/maxBinHeap.js
@@ -5,6 +5,7 @@ class MaxBinaryHeap {
 
   insert (element) {
     this.values.push(element)
+    this.bubbleUp()
   }
   bubbleUp() {
     var index = this.values.length -1
@@ -18,4 +19,46 @@ class MaxBinaryHeap {
       index = parentIndex
     }
   }
+
+  extractMax () {
+    var max = this.values[0];
+    var end = this.values.pop();
+    this.value[0]=end;
+    this.sinkDown()
+    return max;
+  }
+
+  sinkDown () {
+    var index = 0
+    var length = this.values.length
+    var element = this.values[0]
+    while (true) {
+      var parent
+      var leftChildIndex = 2*index +1
+      var rightChildIndex = 2*index +2
+      var leftChild
+      var rightChild
+      var swap = null
+      if (leftChild< length) {
+        leftChild= this.values[leftChildIndex]
+        if(leftChild>element) {
+          swap=leftChildIndex
+        }
+      }
+      if (rightChildIndex<length) {
+        rightChild=this.values[rightChildIndex]
+        if (
+          (swap===null && rightChild> element) ||
+          (swap!== null && rightChild> leftChild)
+        ) {
+          swap = rightChildIndex
+        }
+      }
+      if (swap === null) break
+      this.values[index]=this.values[swap]
+      this.values[swap]=element
+      index = swap
+    }
+  }
+
 }
