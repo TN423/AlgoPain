ec4f28424f67a37fb6b73bdb5db2527be3a858e5
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-11-03 15:07:41 -0400

    Time: 76 ms (95.73%), Space: 41 MB (52.73%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -2,28 +2,39 @@
  * @param {number[][]} intervals
  * @return {number}
  */
+// var minMeetingRooms = function(intervals) {
+//     intervals.sort((a,b)=>{return a[0]-b[0]})
+    
+//     var rooms = []
+    
+//     for (var j=0; j<intervals.length;j++) {
+//         var hasRoom = false
+//         for (var i=0;i<rooms.length;i++) {
+//             //check if any available rooms
+//             if(rooms[i][rooms[i].length-1]<=intervals[j][0]) {
+//                 //if available add the end time to the room
+//                 rooms[i].push(intervals[j][1])
+//                 hasRoom = true
+//                 break
+//             }
+//         }
+//         if(!hasRoom) {
+//             rooms.push([])
+//             rooms[rooms.length-1].push(intervals[j][1])
+//         }
+//     }
+//     console.log(rooms)
+//     return rooms.length
+// };
+// console.log(minMeetingRooms([[2,7]]))
+
 var minMeetingRooms = function(intervals) {
-    intervals.sort((a,b)=>{return a[0]-b[0]})
-    
-    var rooms = []
-    
-    for (var j=0; j<intervals.length;j++) {
-        var hasRoom = false
-        for (var i=0;i<rooms.length;i++) {
-            //check if any available rooms
-            if(rooms[i][rooms[i].length-1]<=intervals[j][0]) {
-                //if available add the end time to the room
-                rooms[i].push(intervals[j][1])
-                hasRoom = true
-                break
-            }
-        }
-        if(!hasRoom) {
-            rooms.push([])
-            rooms[rooms.length-1].push(intervals[j][1])
-        }
-    }
-    console.log(rooms)
-    return rooms.length
+  const start = [...intervals].map(([start, end]) => start).sort((a, b) => a - b);
+  const end = [...intervals].map(([start, end]) => end).sort((a, b) => a - b);
+
+  for (let i = 0; i < intervals.length; i++) {
+    if (start[i] >= end[0]) end.shift();
+  }
+  
+  return end.length;
 };
-console.log(minMeetingRooms([[2,7]]))
