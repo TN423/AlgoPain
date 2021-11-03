120c215dc50e88ca47323f92aa80f492578743c4
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-11-03 17:24:21 -0400

    Time: 76 ms (71.69%), Space: 39.9 MB (36.10%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var stack = []
+    var map = {
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+    for (var i =0; i<s.length; i++) {
+        if(s[i]==='('||s[i]==='{'||s[i]==='[') {
+            stack.push(s[i])
+        } else {
+            if (s[i]!==map[stack[stack.length-1]]) return false
+            stack.pop()
+        }
+    }
+    if (stack.length!==0) return false
+    return true
+    
+    console.log(stack)
+};
