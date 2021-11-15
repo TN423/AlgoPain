782b85e0cbfdbfbff8a42d2c10a6b21b11d06886
Justin <hurstjustina@gmail.com>
2021-11-15 17:56:38 -0500

    graph valid tree

--- graph-valid-tree/graph-valid-tree.js
+++ graph-valid-tree/graph-valid-tree.js
@@ -3,46 +3,30 @@
  * @param {number[][]} edges
  * @return {boolean}
  */
-var validTree = function(n, edges) {
-    if (n < 2) return true;
-    var adj = {};
-
-    // Initalize adjacency list
-    for (var i=0; i<n; i++) {
-        adj[i] = [];
+ var validTree = function(n, edges) {
+    if (n <2 || n===undefined) return true
+    //create adjacency list
+    var adjList = {}
+    for (var i =0; i<n; i++) {
+        adjList[i]=[]
     }
-
-    // Populate adjacency list
     for (var edge of edges) {
-        adj[edge[0]].push(edge[1]);
-        adj[edge[1]].push(edge[0]);
+        adjList[edge[0]].push(edge[1])
+        adjList[edge[1]].push(edge[0])
     }
-
-    // Check if there are cycles
-    var visited = new Set(),
-        parent = {},
-        containsCycles = false;
-    function dfs(root) {
-        visited.add(root);
-        for (var neighbor of adj[root]) {
-            if (visited.has(neighbor)){
-                if (parent[root] !== neighbor) {
-                    // Cycle detected
-                    containsCycles = true;
-                } else {
-                    // Leave parent as is
-                    continue;
-                }
-            } else {
-                // New node
-                parent[neighbor] = root;
-                dfs(neighbor);
-            }
+    //keep track of visited to detect cycles
+    var visit = []
+    //run dfs, but skip the previous node when detecting for cycles, since this is undirected graph
+    function dfs (i, prev) {
+        if (visit.includes(i)) return false
+        visit.push(i)
+        for(var j of adjList[i]) {
+            if (j === prev) continue
+            if (!dfs(j, i)) return false
         }
+        return true
     }
-    dfs(0);
-    if (containsCycles) return false;
+    //visited nodes has to equal n for it to be a tree (only 1 'province')
+    return dfs(0,-1) && visit.length === n
 
-    // Check if the graph is connected (tree contains all nodes)
-    return visited.size === n;
 };
