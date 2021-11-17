6756e35c484fddb5e0e5030f493e6324e75bdcdb
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-11-16 19:47:28 -0500

    Time: 80 ms (95.52%), Space: 42.2 MB (69.51%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.js
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.js
@@ -5,82 +5,89 @@
  */
 
 
-var adjList = function (n, edges) {
-    var adjList = Array.from({length:n},()=>[])
-    for (var edge of edges) {
-        var [src, dest] = edge
-        adjList[src].push(dest)
-        adjList[dest].push(src)
-    }
-    // console.log(adjList)
-    return adjList
-}
+// var adjList = function (n, edges) {
+//     var adjList = Array.from({length:n},()=>[])
+//     for (var edge of edges) {
+//         var [src, dest] = edge
+//         adjList[src].push(dest)
+//         adjList[dest].push(src)
+//     }
+//     // console.log(adjList)
+//     return adjList
+// }
 
-var bFs = function (node, list, visited) {
-    var queue = [node]
-    visited[node]=true
-    while(queue.length) {
-        var curNode = queue.shift()
-        for (var neighbor of list[curNode]) {
-            if(!visited[neighbor]) {
-                visited[neighbor]=true
-                queue.push(neighbor)
-            }
-        }
-    }
-}
+// // var bFs = function (node, list, visited) {
+// //     var queue = [node]
+// //     visited[node]=true
+// //     while(queue.length) {
+// //         var curNode = queue.shift()
+// //         for (var neighbor of list[curNode]) {
+// //             if(!visited[neighbor]) {
+// //                 visited[neighbor]=true
+// //                 queue.push(neighbor)
+// //             }
+// //         }
+// //     }
+// // }
 
-
-var countComponents = function(n, edges) {
-    var list = adjList(n, edges)
-    var visited = {}
-    var numComponents = 0
-    for (var vertex =0; vertex<list.length; vertex++) {
-        if(!visited[vertex]) {
-            numComponents++
-            bFs(vertex, list, visited)
-        }
-    }
-    return numComponents
-}
-
-
-
-
-
-
-
-
-
-
-
+// var dFs = function (node, list, visited) {
+//     visited[node]=true
+//     for (var neighbor of list[node]) {
+//         if(!visited[neighbor]) {
+//             visited[neighbor]=true
+//             dFs(neighbor, list, visited)
+//         }
+//     }
+// }
 
 // var countComponents = function(n, edges) {
-//     var adjList = {}
-//     for (var i=0; i < n;i++){
-//          adjList[i]=[]
-//     }
-//     for (var edge of edges) {
-//         adjList[edge[0]].push(edge[1])
-//         adjList[edge[1]].push(edge[0])
-//     }
-//     var visit = []
-//     var components = 0
-// //     console.log(adjList)
-//     for (var vertex in adjList) {
-//         if (!visit.includes(parseInt(vertex))) {
-//             components++
-//             dfs (parseInt(vertex), -1)
-//         }        
-//     }
-//     function dfs(i, prev) {
-//         if (visit.includes(i)) return false
-//         visit.push(i)
-//         for (var j of adjList[i]) {
-//             if (j===prev) continue
-//             if(!dfs(j,i)) return false
+//     var list = adjList(n, edges)
+//     var visited = {}
+//     var numComponents = 0
+//     for (var vertex =0; vertex<list.length; vertex++) {
+//         if(!visited[vertex]) {
+//             numComponents++
+//             dFs(vertex, list, visited)
 //         }
-//         return true
 //     }
-//     return components
+//     return numComponents
 // }
+
+//union find verison
+var countComponents = function (n, edges) {
+    var parent = []
+    for (var i =0; i<n;i++){
+        parent.push(i)
+    }
+    var rank = Array(n).fill().map(()=>1)
+    
+    //find a node's root parent
+    function find (n1) {
+        var res = n1
+        while (res !== parent[res]) {
+            parent[res] = parent[parent[res]]
+            res = parent[res]
+        }
+        return res
+    }
+    function union (n1, n2) {
+        var p1 = find(n1)
+        var p2 = find(n2)
+        if (p1 === p2) return 0
+        
+        if (rank[p2]>rank[p1]) {
+            parent[p1]=p2
+            rank[p2] += rank[p1]
+        } else {
+            parent[p2]=p1
+            rank[p1]+= rank[p2]
+        }
+        return 1
+    }
+    var res = n
+    for (var edge of edges) {
+        res -= union(edge[0],edge[1])
+    }
+    return res
+}
+
