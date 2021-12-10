44dd26e7a56a5e56ade3b16f905ca3871bbecd4f
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-12-10 16:57:07 -0500

    Time: 176 ms (13.67%), Space: 47.3 MB (10.27%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+
+var buildAdjList = function (number, edges) {
+    var adjList = Array(number).fill().map(()=>[])
+    for (var edge of edges) {
+        var [src,dest]=edge
+        adjList[src].push(dest)
+    }
+    // console.log(adjList)
+    return adjList
+}
+
+var dFs = function (node, adjList, visiting, visited, result) {
+    // console.log('this adjlist',adjList)
+    if (visiting.has(node)) return false
+    if (visited.has(node)) return true
+    visiting.add(node)
+    console.log('this node',node)
+    for (var neighbor of adjList[node]) {
+        if (!dFs(neighbor, adjList, visiting, visited, result)) return false
+    }
+    visiting.delete(node)
+    visited.add(node)
+    result.push(node)
+    return true
+}
+
+var findOrder = function(numCourses, prerequisites) {
+  var adjList = buildAdjList(numCourses, prerequisites)
+  // console.log(adjList)
+  var visiting1 = new Set()
+  var visited1 = new Set()
+  var result = []
+  for (var i=0; i<adjList.length;i++) {
+    // console.log(node)  
+    console.log('this one',visiting1)  
+    if (!dFs(i, adjList, visiting1, visited1,result)) return []  
+  }    
+  return result  
+};
