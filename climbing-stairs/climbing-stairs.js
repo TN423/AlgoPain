205b9ccf5ee4fa9711cdf22424be22b4d4fe2a53
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-12-11 22:58:06 -0500

    Time: 101 ms (15.17%), Space: 39.9 MB (9.52%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    var dp = []
+    var dp = Array(n+1).fill(0)
     dp[0]=1
     dp[1]=1
-    for (var i=2;i<=n;i++) {
+    console.log(dp)
+    for (var i=2;i<dp.length;i++) {
         dp[i]=dp[i-1]+dp[i-2]
     }
-    return dp[n]
+    return dp[dp.length-1]
 };
