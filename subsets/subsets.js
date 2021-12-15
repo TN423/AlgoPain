3bf8a4912aa64e2cccc05f056d291075b9b85f3b
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-12-15 01:08:56 -0500

    Time: 98 ms (25.01%), Space: 40.5 MB (98.51%) - LeetHub

---
+++ subsets/subsets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  var result = []
+  var dfs = function (i, nums, slate) {
+      if (i === nums.length) {
+          result.push(slate.slice())
+          return
+      }
+      //exclude
+      dfs(i+1, nums, slate)
+      
+      //include
+      slate.push(nums[i])
+      dfs(i+1, nums, slate)
+      slate.pop()
+  }
+  dfs(0, nums, [])
+  console.log(result)  
+  return result  
+};
