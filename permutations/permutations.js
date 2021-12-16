7f318059269c68361bce99119c1e83a770ca327c
Justin Hurst <80915187+TN423@users.noreply.github.com>
2021-12-16 02:48:01 -0500

    Time: 131 ms (16.82%), Space: 41.8 MB (62.13%) - LeetHub

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -2,21 +2,34 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var permute = function(nums) {
+//    var resArray = []
+//    var build = function (current,remainder) {
+//        if (remainder.length ===0) {
+//            resArray.push(current)
+//        }
+// //        console.log(remainder)
+//        for (var i =0; i< remainder.length;i++) {
+//          build(current.concat(remainder[i]),remainder.slice(0,i).concat(remainder.slice(i+1)))   
+//        }
+//    }
+//    build([],nums)
+//    return resArray 
+// };
+
 var permute = function(nums) {
-   var resArray = []
-   var build = function (current,remainder) {
-       if (remainder.length ===0) {
-           resArray.push(current)
-       }
-//        console.log(remainder)
-       for (var i =0; i< remainder.length;i++) {
-         build(current.concat(remainder[i]),remainder.slice(0,i).concat(remainder.slice(i+1)))   
-       }
-   }
-   build([],nums)
-   return resArray 
+  var result = []
+  var dfs = (i, nums)=> {
+      if (i===nums.length) {
+          result.push(nums.slice())
+          return
+      }
+      for (var j=i; j<nums.length;j++) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+          dfs(i+1, nums);
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+  }
+  dfs(0,nums)
+  return result
 };
-
-var test = [1,2,3]
-
-permute(test)
