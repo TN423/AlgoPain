6c834c5c6155ccb9987ee174de0b4d9474192b0e
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-14 15:34:12 -0500

    Time: 68 ms (93.11%), Space: 39.2 MB (30.30%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  var i = m-1
+  var j = n-1
+  for (var k=nums1.length-1; k>=0;k--){
+      if (j<0) break
+      if (nums1[i]> nums2[j]) {
+          nums1[k]=nums1[i]
+          i--
+      } else {
+          nums1[k]=nums2[j]
+          j--
+      }
+  }
+};
