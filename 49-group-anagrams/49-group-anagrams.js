293ef53220d699328e0554ff02f558a79572a86b
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-18 18:24:23 -0500

    Time: 124 ms (91.98%), Space: 50 MB (62.23%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,19 +3,33 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    //hash table with key sorted string, value empty array
-    var hashTable = {}
-    for (var word of strs) {
-        //.split('').sort().join() to get hash table of sorted words
-        var sorted = word.split('').sort().join('')
-        if (!hashTable[sorted]) {
-            hashTable[sorted]=[]
-            hashTable[sorted].push(word)
-        } else {
-            hashTable[sorted].push(word)
-        }
+  var hash ={}
+  for (var word of strs) {
+      var sorted = word.split('').sort().join('')
+      if (!hash[sorted]) {
+          hash[sorted]=[word]
+      } else {
+          hash[sorted].push(word)
+      }
+  } 
+  return Object.values(hash)  
+}
+
+
+// var groupAnagrams = function(strs) {
+//     //hash table with key sorted string, value empty array
+//     var hashTable = {}
+//     for (var word of strs) {
+//         //.split('').sort().join() to get hash table of sorted words
+//         var sorted = word.split('').sort().join('')
+//         if (!hashTable[sorted]) {
+//             hashTable[sorted]=[]
+//             hashTable[sorted].push(word)
+//         } else {
+//             hashTable[sorted].push(word)
+//         }
         
-    }
-    //return an array of the values of the hash table
-    return Object.values(hashTable)
-};
+//     }
+//     //return an array of the values of the hash table
+//     return Object.values(hashTable)
+// };
