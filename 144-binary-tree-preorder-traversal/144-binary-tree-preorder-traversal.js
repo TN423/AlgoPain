d90aa7d85c674123b5be1b90661ddc32c8fb2aa5
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-21 18:56:08 -0500

    Time: 128 ms (9.04%), Space: 38.9 MB (45.88%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -10,14 +10,32 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+
+//depth first
+// var preorderTraversal = function(root) {
+//     var result = []
+//     var dfs = function (node) {
+//         if (!node) return
+//         result.push(node.val)
+//         dfs(node.left)
+//         dfs(node.right)
+//     }
+//     dfs(root)
+//     return result
+// };
+
+
+//breadth first
 var preorderTraversal = function(root) {
+   if (!root) return []
     var result = []
-    var dfs = function (node) {
-        if (!node) return
-        result.push(node.val)
-        dfs(node.left)
-        dfs(node.right)
-    }
-    dfs(root)
-    return result
+   var stack = [root]
+   while (stack.length) {
+       var node = stack.pop()
+       result.push(node.val)
+       if (node.right) stack.push(node.right)
+       if (node.left) stack.push(node.left)
+       
+   }
+   return result 
 };
