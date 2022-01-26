fd621dc2676dc708fe2850f4efc8ff1b82980ed6
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-26 00:01:53 -0500

    Time: 384 ms (5.14%), Space: 48.5 MB (38.69%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+  if (!root) return []
+  var result = []
+  var dfs = function (node, sum, slate) {
+      if (node.left === null && node.right === null) {
+          slate.push(node.val)
+          if (node.val === sum) {
+              result.push(slate.slice())
+          }
+          slate.pop()
+          return
+      }
+      slate.push(node.val)
+      console.log(slate)
+      if (node.left) dfs (node.left, sum - node.val, slate)
+      slate.pop()
+      
+      slate.push(node.val)
+      if (node.right) dfs (node.right, sum - node.val, slate)
+      slate.pop()
+  }
+  dfs(root, targetSum, [])
+  return result  
+};
