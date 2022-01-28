ae9166347b94d4ec15bfa2125a4aac7d40df8354
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-27 22:34:41 -0500

    Time: 64 ms (100.00%), Space: 44.6 MB (5.41%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.js
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.js
@@ -12,7 +12,7 @@
  */
 var countUnivalSubtrees = function(root) {
   if (!root) return 0
-    var count = 0
+  var count = 0
   var dfs = function (node) {
       if (node.left === null && node.right === null) {
           count++
@@ -21,6 +21,10 @@ var countUnivalSubtrees = function(root) {
       
       var unival = true
       
+      
+      //if one one node returns a false, the parent can't be a unival even if the other node 
+      //returns true, this is why we need the unival = unival - to prevent it from resetting to to true when we run dfs on the other node (i.e left is false, but right is true) it our local variable will reset to true unless we say unival = unival && left/right && left/right val = current node.val
+      
       if (node.left) {
           var left = dfs(node.left)
           unival = unival && left && node.left.val === node.val
@@ -28,7 +32,7 @@ var countUnivalSubtrees = function(root) {
       
       if (node.right) {
           var right = dfs(node.right)
-          unival = unival && right && node.right.val === node.val
+          unival =  unival &&right && node.right.val === node.val
       }
       if (unival) count++
       return unival
