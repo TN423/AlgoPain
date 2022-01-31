5737545cbf63c7243e3cac1624a6ea99b1d962df
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-31 13:22:34 -0500

    Time: 75 ms (95.41%), Space: 44.5 MB (5.07%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+
+var hasCycle = function(head) {
+    var slow = head;
+    var fast = head;
+    while (fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
+        if (fast === slow) return true
+    }
+    return false
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var hasCycle = function(head) {
+//     var map = new Map()
+//     while (head && map.get(head)===undefined){
+//         map.set(head, head.val)
+//         head = head.next
+//     }
+//     if (map.get(head)!==undefined) return true
+//     return false
+// };
