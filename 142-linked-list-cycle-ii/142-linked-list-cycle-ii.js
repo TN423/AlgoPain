850d8ed40a2aae5b16600ab1003ecd4f1f198d4a
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-31 18:18:58 -0500

    Time: 117 ms (45.89%), Space: 45.4 MB (5.02%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  if (!head) return null
+  if (!head.next) return null  
+  var slow = head
+  var fast = head
+  var start = head
+  while (fast && fast.next) {
+      fast = fast.next.next
+      slow = slow.next
+      if (slow === fast) break
+  }
+  if (fast !== slow) return null  
+    
+  while (slow !== start) {
+      
+      start = start.next
+      slow = slow.next
+  }  
+    return slow
+}
