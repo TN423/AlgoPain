2b901f911bf5baed65d4165d3c5fd26d6085308f
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-01-31 11:58:14 -0500

    Time: 77 ms (41.09%), Space: 42 MB (5.05%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    var front = head
+    var array = []
+    while (head) {
+        array.push(head.val)
+        head = head.next
+    }
+    var mid = Math.ceil((array.length-1)/2)
+    while (mid) {
+        front = front.next
+        mid--
+    }
+
+    return front
+};
