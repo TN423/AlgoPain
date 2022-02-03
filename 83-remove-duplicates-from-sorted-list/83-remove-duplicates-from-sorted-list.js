fea961999fc074398b8b4c9454d59209c0cd0c6e
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-03 16:09:59 -0500

    Time: 126 ms (32.15%), Space: 44.3 MB (7.92%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    var dummy = new ListNode (-Infinity, head)
+    var prev = dummy
+    var cur = head
+    while (cur) {
+        if (prev.val === cur.val) {
+            while (cur && cur.val === prev.val){
+                cur=cur.next
+            }
+        }
+        prev.next = cur
+        prev = prev.next
+        if (cur) cur =cur.next
+    }
+    return dummy.next
+};
