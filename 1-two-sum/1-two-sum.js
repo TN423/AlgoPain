cf7e2f7109be14524d3a08d006daa26bf6b332fe
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-09 15:17:49 -0500

    Time: 68 ms (97.74%), Space: 45.1 MB (5.34%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,6 +3,10 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//others are brute force and 2 pointer
+//2 pointer - first sort the array, then  is 1 at start , 1 at last, decrement last if result greater than target, increment start if result smaller than target
+//hash table solution - has table where nums values are keys and indices are values
 var twoSum = function(nums, target) {
     var hash =  {}
     for (var i=0; i<nums.length;i++) {
