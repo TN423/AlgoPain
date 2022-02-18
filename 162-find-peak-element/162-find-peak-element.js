f14de2499ce408aab388b42cd5715327a31ac4fd
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-18 14:35:27 -0500

    Time: 66 ms (86.78%), Space: 42.2 MB (19.72%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    var left = 0 
+    var right = nums.length -1
+    while (left < right) {
+        var mid = Math.floor((left+right)/2)
+        if (nums[mid]>nums[mid+1]) {
+            right = mid
+        } else {
+            left = mid +1
+        }
+    }
+    return left
+    
+};
