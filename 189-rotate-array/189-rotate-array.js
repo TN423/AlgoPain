917e0541b839dbcc431cd8d1f1805e45eda38db0
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-23 13:16:28 -0500

    Time: 136 ms (56.20%), Space: 52.5 MB (24.24%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+//reverse the whole thing, then reverse the subsections, defined by the kth element
+
+
+var reverse = function (nums, start, end) {
+
+    while (start < end) {
+        var temp = nums[start]
+        nums[start]=nums[end]
+        nums[end]=temp
+        // [nums[start], nums[end] = nums[end], nums[start]];
+        start++
+        end--
+    }
+}
+
+
+var rotate = function(nums, k) {
+    k = k % nums.length
+    
+    nums.reverse()
+    reverse(nums, 0, k-1)
+    reverse(nums, k, nums.length-1)
+    
+};
