43e5e9e35c15204f3fdb984d0fdc2b643065d24a
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-24 17:00:22 -0500

    Time: 93 ms (47.07%), Space: 42 MB (39.29%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -5,17 +5,37 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
 var merge = function(nums1, m, nums2, n) {
-  var i = m-1
-  var j = n-1
-  for (var k=nums1.length-1; k>=0;k--){
-      if (j<0) break
-      if (nums1[i]> nums2[j]) {
-          nums1[k]=nums1[i]
-          i--
+  var f = m-1
+  var s = n-1
+  for (var i=nums1.length-1;i>=0;i--) {
+      if (s<0) break
+      if (nums2[s]<nums1[f]) {
+          nums1[i]=nums1[f]
+          f--
+        
       } else {
-          nums1[k]=nums2[j]
-          j--
-      }
-  }
-};
+            nums1[i]=nums2[s]
+          s--
+    }
+  }    
+}
+
+
+
+// var merge = function(nums1, m, nums2, n) {
+//   //creating 3 pointers, iterating from end of nums
+//   var i = m-1
+//   var j = n-1
+//   for (var k=nums1.length-1; k>=0;k--){
+//       if (j<0) break
+//       if (nums1[i]> nums2[j]) {
+//           nums1[k]=nums1[i]
+//           i--
+//       } else {
+//           nums1[k]=nums2[j]
+//           j--
+//       }
+//   }
+// };
