b46ef4c3a0b7784164fa7bb6a97db760e8ac7a6c
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-24 14:20:28 -0500

    Time: 154 ms (47.89%), Space: 61 MB (14.57%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -2,19 +2,59 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//floyds algorithm
+//its a linked list cycle problem.
+//first detect cycle, then start a fres pointer and return the index where it meets the slow pointer
+
 var findDuplicate = function(nums) {
-    var slow =0
-    var fast = 0
+    var fast = 0;
+    var slow = 0;
     while (true) {
+        fast = nums[[nums[fast]]]
         slow = nums[slow]
-        fast = nums[nums[fast]]
         if (slow === fast) {
-            var start = 0
+            var start =0
             while (start !== slow) {
                 slow = nums[slow]
                 start = nums[start]
-                if (slow === start) return start
+                if (start === slow) return start
             }
         }
     }
-};
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var findDuplicate = function(nums) {
+//     var slow =0
+//     var fast = 0
+//     while (true) {
+//         slow = nums[slow]
+//         fast = nums[nums[fast]]
+//         if (slow === fast) {
+//             var start = 0
+//             while (start !== slow) {
+//                 slow = nums[slow]
+//                 start = nums[start]
+//                 if (slow === start) return start
+//             }
+//         }
+//     }
+// };
