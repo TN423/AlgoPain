2ff2e3130ebc819425b0f32d12ff9483f1c4b090
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-23 19:01:42 -0500

    Time: 92 ms (68.86%), Space: 44.1 MB (49.92%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,23 +3,32 @@
  * @return {number}
  */
 
+//O(n) time but O(1) space
 var missingNumber = function(nums) {
-    var hash = {}
-    for (var num of nums) {
-        hash[num]=true
-    }
+  var gSum = (nums.length * (nums.length+1)/2)
+  var nSum = nums.reduce((accum, el)=>accum+el, 0)
+  return gSum-nSum
+}
+
+
+//O(N) time solution using hash table, O(n) space
+// var missingNumber = function(nums) {
+//     var hash = {}
+//     for (var num of nums) {
+//         hash[num]=true
+//     }
     
-    var count = nums.length
-    while (count>=0) {
-        if (!hash[count]) return count
-        count --    
-    }
+//     var count = nums.length
+//     while (count>=0) {
+//         if (!hash[count]) return count
+//         count --    
+//     }
     
-};
+// };
 
 
 
-
+// N LOG N Solution
 // var missingNumber = function(nums) {
 //     nums.sort((a,b)=>a-b);
 //     var count = 0;
