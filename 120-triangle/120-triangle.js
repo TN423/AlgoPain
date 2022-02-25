7a20cc50f8954d06868c3c56187626a0e786b7e7
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-25 15:48:31 -0500

    Time: 75 ms (83.66%), Space: 43.8 MB (19.76%) - LeetHub

--- 120-triangle/120-triangle.js
+++ 120-triangle/120-triangle.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
+    //tabulate a result array of all the sums
     var result = []
     result[0]=triangle[0]
     for (var i=1;i<triangle.length;i++) {
@@ -13,10 +14,11 @@ var minimumTotal = function(triangle) {
         result[i][triangle[i].length-1]=triangle[i][triangle[i].length-1]+result[i-1][triangle[i-1].length-1]
     }
     
-    var min = Infinity
-    for (var k=0;k<result[result.length-1].length;k++){
-        min=Math.min(min, result[result.length-1][k])
-    }
-    return min
+    // var min = Infinity
+    // //scan the last index of the result array for the min
+    // for (var k=0;k<result[result.length-1].length;k++){
+    //     min=Math.min(min, result[result.length-1][k])
+    // }
+    return Math.min(...result[result.length-1])
 
 };
