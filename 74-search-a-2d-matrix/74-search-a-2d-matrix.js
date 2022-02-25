e9d908fc6791d2eef63d74e04619ab8179747449
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-25 12:54:05 -0500

    Time: 98 ms (41.16%), Space: 42.6 MB (11.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -3,22 +3,65 @@
  * @param {number} target
  * @return {boolean}
  */
+
+
 var searchMatrix = function(matrix, target) {
     var row = matrix.length;
     var col = matrix[0].length;
     var left = 0;
-    var right = row*col -1
+    var right = row * col -1;
     while (left <= right) {
-        var mid = Math.floor((left + right)/2)
-        var midNum = matrix[Math.floor(mid/col)][mid%col]
-        if (midNum === target) return true
-        if (target < midNum) {
-            right = mid -1
+        var mid = Math.floor((left+right)/2);
+        var midVal = matrix[Math.floor(mid/col)][mid % col]
+        if (midVal===target) return true
+        if (target < midVal) {
+            right = mid-1
         } else {
             left = mid+1
-        }    
+        }
     }
-    
     return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var searchMatrix = function(matrix, target) {
+//     var row = matrix.length;
+//     var col = matrix[0].length;
+//     var left = 0;
+//     var right = row*col -1
+//     while (left <= right) {
+//         var mid = Math.floor((left + right)/2)
+//         var midNum = matrix[Math.floor(mid/col)][mid%col]
+//         if (midNum === target) return true
+//         if (target < midNum) {
+//             right = mid -1
+//         } else {
+//             left = mid+1
+//         }    
+//     }
     
-};
+//     return false
+    
+// };
