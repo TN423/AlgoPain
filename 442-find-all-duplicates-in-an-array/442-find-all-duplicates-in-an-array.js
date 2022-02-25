a91adbd719dadc18938905b5641730bf7f934e78
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-02-25 01:34:01 -0500

    Time: 121 ms (78.00%), Space: 49 MB (76.13%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -2,21 +2,57 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+
+//key hint is = all the integers of nums are in the range [1, n]
+//you can make use of the indices like a link list, value 4 points to index 4, value of 1 points to index 1, etc
+//if you come back to the same index more than once, its a duplicate
+
+
 var findDuplicates = function(nums) {
-    var hash =  {};
-    var result = []
-    for (var num of nums) {
-        if (!hash[num]) {
-            hash[num]=1
+    var result = [];
+    for (var i =0; i <nums.length;i++) {
+        var index = Math.abs(nums[i])-1;
+        var val = nums[index]
+        if (val <0) {
+            result.push(Math.abs(nums[i]))
         } else {
-            hash[num]++
+            nums[index]=-nums[index]
         }
     }
-    
-    for (var key in hash) {
-        if (hash[key]>1) result.push(key)
-    }
-    
     return result
     
-};
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//uses hash table
+// var findDuplicates = function(nums) {
+//     var hash =  {};
+//     var result = []
+//     for (var num of nums) {
+//         if (!hash[num]) {
+//             hash[num]=1
+//         } else {
+//             hash[num]++
+//         }
+//     }
+    
+//     for (var key in hash) {
+//         if (hash[key]>1) result.push(key)
+//     }
+    
+//     return result
+    
+// };
