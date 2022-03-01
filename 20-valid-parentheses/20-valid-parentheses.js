c2677e3551d3bcc4b20de536376cb9e2dbdc158b
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-03-01 18:48:05 -0500

    Time: 85 ms (61.36%), Space: 42.3 MB (30.40%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -4,29 +4,72 @@
  */
 
 var isValid = function(s) {
-    var stack=[]
-    var dict = {
-        '{':'}',
-        '(':')',
-        '[':']'
-    }
-    for (var i=0;i<s.length;i++) {
-        if (s[i]==='{' || s[i]==='(' || s[i]==='[' ) {
-            stack.push(s[i])
-        } else {
-            if (dict[stack[stack.length-1]]!==s[i]) return false
+    var stack = [];
+    var dict = '[] {} ()'
+    var i =0
+    while(i <s.length) {
+        stack.push(s[i])
+        i++
+        var closed = stack[stack.length-1]
+        var open = stack[stack.length-2]
+        var combined = open+closed
+        if (dict.includes(combined)) {
+            stack.pop()
             stack.pop()
         }
     }
-    
-    if (stack.length) return false
-    
-    return true
-     
-    
+    return stack.length === 0
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var isValid = function(s) {
+//     var stack=[]
+//     var dict = {
+//         '{':'}',
+//         '(':')',
+//         '[':']'
+//     }
+//     for (var i=0;i<s.length;i++) {
+//         if (s[i]==='{' || s[i]==='(' || s[i]==='[' ) {
+//             stack.push(s[i])
+//         } else {
+//             if (dict[stack[stack.length-1]]!==s[i]) return false
+//             stack.pop()
+//         }
+//     }
+    
+//     if (stack.length) return false
+    
+//     return true
+// }
 
 
 
