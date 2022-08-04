13b177abc751e029b4037972095578bf1e7ae5ab
Justin Hurst <80915187+TN423@users.noreply.github.com>
2022-08-04 17:17:35 -0400

    Time: 129 ms (52.02%), Space: 49.1 MB (27.78%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.js
+++ 210-course-schedule-ii/210-course-schedule-ii.js
@@ -3,40 +3,37 @@
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-
-var buildAdjList = function (number, edges) {
-    var adjList = Array(number).fill().map(()=>[])
-    for (var edge of edges) {
-        var [src,dest]=edge
-        adjList[src].push(dest)
-    }
-    // console.log(adjList)
-    return adjList
+ var buildAdjList = function (number, edges) {
+  var adjList = Array(number).fill().map(()=>[])
+  for (var edge of edges) {
+      var [src,dest]=edge
+      adjList[src].push(dest)
+  }
+  // console.log(adjList)
+  return adjList
 }
 
 var dFs = function (node, adjList, visiting, visited, result) {
-    if (visiting[node]) return false
-    if (visited[node]) return true
-    visiting[node]=true
-    visited[node]=true
-    for (var neighbor of adjList[node]) {
-        if (!dFs(neighbor, adjList, visiting, visited, result)) return false
-    }
-    visiting[node]=false
-    
-    result.push(node)
-    return true
+  if (visiting[node]) return false
+  if (visited[node]) return true
+  visiting[node]=true
+  for (var neighbor of adjList[node]) {
+      if (!dFs(neighbor, adjList, visiting, visited, result)) return false
+  }
+  visiting[node]=false
+  visited[node]=true
+  result.push(node)
+  return true
 }
 
 var findOrder = function(numCourses, prerequisites) {
-  var adjList = buildAdjList(numCourses, prerequisites)
-  var visiting1 = {}
-  var visited1 = {}
-  var result = []
-  
-  for (var i=0; i<adjList.length;i++) {
-    // console.log(node)  
-    if (!dFs(i, adjList, visiting1, visited1,result)) return []  
-  }    
-  return result  
+var adjList = buildAdjList(numCourses, prerequisites)
+var visiting1 = {}
+var visited1 = {}
+var result = []
+for (var i=0; i<adjList.length;i++) {
+  // console.log(node)
+  if (!dFs(i, adjList, visiting1, visited1,result)) return []
+}
+return result
 };
