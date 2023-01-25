1618f8e0d77249bbede04615f8afdfc61dcf4692
Justin Hurst <80915187+TN423@users.noreply.github.com>
2023-01-25 00:25:07 -0500

    Time: 171 ms (14.29%), Space: 49.2 MB (21.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -5,36 +5,39 @@
  */
 
 var buildAdjList = function (number, edges) {
-  var adjList = Array(number).fill().map(()=>[])
-  for (var edge of edges) {
-      var [src,dest]=edge
-      adjList[src].push(dest)
-  }
-  // console.log(adjList)
-  return adjList
+    var adjList = Array(number).fill().map(()=>[])
+    for (var edge of edges) {
+        var [src,dest]=edge
+        adjList[src].push(dest)
+    }
+    // console.log(adjList)
+    return adjList
 }
 
 var dFs = function (node, adjList, visiting, visited, result) {
-  if (visiting[node]) return false
-  if (visited[node]) return true
-  visiting[node]=true
-  for (var neighbor of adjList[node]) {
-      if (!dFs(neighbor, adjList, visiting, visited, result)) return false
-  }
-  visiting[node]=false
-  visited[node]=true
-  result.push(node)
-  return true
+    if (visiting[node]) return false
+    if (visited[node]) return true
+    visiting[node]=true
+    for (var neighbor of adjList[node]) {
+        if (!visited[neighbor]) {
+            if(!dFs(neighbor,adjList, visiting, visited, result)) return false
+        }
+    }
+    visiting[node]=false
+    visited[node]=true 
+    result.push(node)
+    return true
 }
 
 var findOrder = function(numCourses, prerequisites) {
-var adjList = buildAdjList(numCourses, prerequisites)
-var visiting1 = {}
-var visited1 = {}
-var result = []
-for (var i=0; i<adjList.length;i++) {
-  // console.log(node)
-  if (!dFs(i, adjList, visiting1, visited1,result)) return []
-}
-return result
+  var adjList = buildAdjList(numCourses, prerequisites)
+  var visiting1 = {}
+  var visited1 = {}
+  var result = []
+  
+  for (var i=0; i<adjList.length;i++) {
+    // console.log(node)  
+    if (!dFs(i, adjList, visiting1, visited1,result)) return []  
+  }    
+  return result  
 };
